45b654cd2076bdc55119d259759e7d172c497e94
Cowation <cow2cow4cow@gmail.com>
2022-05-17 19:10:42 -0400

    Edit server config to use actual server

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://stucosurveys.vercel.app';
+  : 'https://stuco-surveys.vercel.app';
