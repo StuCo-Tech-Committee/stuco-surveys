3712b01299e0054668271f96396bd9fa0d150d73
Cowation <cow2cow4cow@gmail.com>
2022-05-21 16:34:03 -0400

    Add a sick loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import Header from '../components/header';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
+      <NextNProgress color="#9A1D2E" options={{ showSpinner: false }} />
       <AnimatePresence>
         {pageProps.hasOwnProperty('header') && pageProps.header == false ? (
           <></>
