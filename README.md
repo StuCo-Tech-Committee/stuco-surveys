a9524d5b25d954e3e6ac756190fa78e0dacb5053
Cowation <cow2cow4cow@gmail.com>
2022-11-04 16:50:41 -0400

    Add Vercel sponsorship criteria

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # StuCo Surveys
 
 Real-time survey system for Student Council.
+
+[![Powered by Vercel](/readme/powered-by-vercel.svg)](https://vercel.com/stuco-tech-committee?utm_source=stuco-tech-committee&utm_campaign=oss)
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { BsInstagram } from 'react-icons/bs';
 import { IconType } from 'react-icons/lib';
@@ -28,11 +29,21 @@ const Footer = () => {
   return (
     <>
       <div className="flex w-full flex-row justify-between bg-gray-100 px-4 py-16 align-top md:px-32">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <Link href="/">
             <a className="text-2xl font-bold text-neutral-800">StuCo Surveys</a>
           </Link>
-          <div></div>
+          <Link href="https://vercel.com/stuco-tech-committee?utm_source=stuco-tech-committee&utm_campaign=oss">
+            <a className="flex flex-row items-center gap-2">
+              <span>Powered by</span>
+              <Image
+                src="/images/vercel-logotype-dark.svg"
+                alt="Vercel logo"
+                width={90}
+                height={20}
+              />
+            </a>
+          </Link>
         </div>
         <div className="flex flex-col justify-start gap-2">
           <h1 className="text-lg text-neutral-800">Platform</h1>
