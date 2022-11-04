36296fb62aa88d260c9303f33663a5cd7d6283b4
Cowation <cow2cow4cow@gmail.com>
2022-11-04 16:44:29 -0400

    Remove unused package @react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { PusherProvider } from '@harelpls/use-pusher';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { AnimatePresence, motion } from 'framer-motion';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
@@ -11,39 +11,35 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
-  const client = new QueryClient();
 
   return (
-    <QueryClientProvider client={client}>
-      <PusherProvider
-        {...{
-          clientKey: '3800fa093dc2b07f5524',
-          cluster: 'us2',
-        }}
-      >
-        <SessionProvider session={session}>
-          <NextNProgress color="#9A1D2E" options={{ showSpinner: false }} />
-          <AnimatePresence>
-            {pageProps.hasOwnProperty('header') &&
-            pageProps.header === false ? (
-              <></>
-            ) : (
-              <Header key="Header" />
-            )}
-          </AnimatePresence>
-          <AnimatePresence exitBeforeEnter>
-            <motion.div key={router.pathname}>
-              <Component {...pageProps} />
-            </motion.div>
-          </AnimatePresence>
+    <PusherProvider
+      {...{
+        clientKey: '3800fa093dc2b07f5524',
+        cluster: 'us2',
+      }}
+    >
+      <SessionProvider session={session}>
+        <NextNProgress color="#9A1D2E" options={{ showSpinner: false }} />
+        <AnimatePresence>
           {pageProps.hasOwnProperty('header') && pageProps.header === false ? (
             <></>
           ) : (
-            <Footer />
+            <Header key="Header" />
           )}
-        </SessionProvider>
-      </PusherProvider>
-    </QueryClientProvider>
+        </AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div key={router.pathname}>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+        {pageProps.hasOwnProperty('header') && pageProps.header === false ? (
+          <></>
+        ) : (
+          <Footer />
+        )}
+      </SessionProvider>
+    </PusherProvider>
   );
 }
 
