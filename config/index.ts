6e4b773ad22fac3c5112007aa5207dc2186389e7
Cowation <cow2cow4cow@gmail.com>
2022-11-07 18:21:04 -0500

    Hotfix for detecting base url

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
+export const server = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : dev
   ? 'http://localhost:3000'
   : 'https://stuco-surveys.vercel.app';
