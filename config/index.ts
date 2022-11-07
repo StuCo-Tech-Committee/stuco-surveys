e20e29d92c138dbdbef607309d0c0bce16219d4a
Cowation <cow2cow4cow@gmail.com>
2022-11-07 18:23:01 -0500

    Switch to NEXT_PUBLIC_VERCEL_URL

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+export const server = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : dev
   ? 'http://localhost:3000'
   : 'https://stuco-surveys.vercel.app';
