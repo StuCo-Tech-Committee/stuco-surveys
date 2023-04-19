64b1d6533ba2f6c9c51e479c4a86f71f4b78df1b
Eric Li <ericlmtn@gmail.com>
2023-04-19 12:43:06 -0400

    Finished Hero section

---
+++ src/components/common/Hero.tsx
@@ -0,0 +1,144 @@
+import { useId } from 'react';
+
+import { Button } from './Button';
+import { Container } from './Container';
+import { PhoneFrame } from './PhoneFrame';
+import logoSoTA from '../../images/icons/SoTA.png';
+import imageSampleSurvey from '../../images/sampleSurvey.png';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { Logo } from './Logo';
+
+function BackgroundIllustration(props: React.ComponentProps<'div'>) {
+  let id = useId();
+
+  return (
+    <div {...props}>
+      <svg
+        viewBox="0 0 1026 1026"
+        fill="none"
+        aria-hidden="true"
+        className="absolute inset-0 h-full w-full animate-spin-slow"
+      >
+        <path
+          d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
+          stroke="#D4D4D4"
+          strokeOpacity="0.7"
+        />
+        <path
+          d="M513 1025C230.23 1025 1 795.77 1 513"
+          stroke={`url(#${id}-gradient-1)`}
+          strokeLinecap="round"
+        />
+        <defs>
+          <linearGradient
+            id={`${id}-gradient-1`}
+            x1="1"
+            y1="513"
+            x2="1"
+            y2="1025"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#9A1D2E" />
+            <stop offset="1" stopColor="#9A1D2E" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <svg
+        viewBox="0 0 1026 1026"
+        fill="none"
+        aria-hidden="true"
+        className="absolute inset-0 h-full w-full animate-spin-reverse-slower"
+      >
+        <path
+          d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
+          stroke="#D4D4D4"
+          strokeOpacity="0.7"
+        />
+        <path
+          d="M913 513c0 220.914-179.086 400-400 400"
+          stroke={`url(#${id}-gradient-2)`}
+          strokeLinecap="round"
+        />
+        <defs>
+          <linearGradient
+            id={`${id}-gradient-2`}
+            x1="913"
+            y1="513"
+            x2="913"
+            y2="913"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#9A1D2E" />
+            <stop offset="1" stopColor="#9A1D2E" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+export function Hero() {
+  return (
+    <div className="overflow-hidden py-20 sm:py-32 lg:pb-32 xl:pb-36">
+      <Container>
+        <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
+          <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
+            <h1 className="text-4xl font-medium tracking-tight text-gray-900">
+              Your voice matters to us.
+            </h1>
+            <p className="mt-6 text-lg text-gray-600">
+              Answer meaningful questions that help us understand you, and the
+              community around you, so that StuCo can better represent the
+              campus.
+            </p>
+            <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
+              {/* // TODO Replace this URL */}
+              <Button
+                href="https://stuco-surveys.vercel.app/survey/643b4ffc508c1b3ac591a740"
+                variant="outline"
+              >
+                View a sample Survey
+              </Button>
+            </div>
+          </div>
+          <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
+            <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
+            <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
+              <PhoneFrame className="mx-auto max-w-[366px]" priority>
+                <div className="'flex flex-col">
+                  <div className="flex items-center justify-center p-2">
+                    <Logo className="h-6 flex-none" />
+                  </div>
+                  <Image src={imageSampleSurvey} alt={'Sample Survey'} />
+                </div>
+              </PhoneFrame>
+            </div>
+          </div>
+          <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
+            <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
+              As featured in
+            </p>
+            <ul
+              role="list"
+              className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
+            >
+              {[['SoTA', logoSoTA, 'lg:col-span-2']].map(
+                ([name, logo, className]) => (
+                  <li key={name.toString()} className={clsx('flex', className)}>
+                    <Image
+                      src={logo}
+                      alt={name.toString()}
+                      className="h-16 w-auto"
+                      unoptimized
+                    />
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         sans: ['var(--font-inter)', 'sans-serif'],
         monospace: ['var(--font-jetbrains-mono)', 'monospace'],
       },
+      animation: {
+        'spin-slow': 'spin 4s linear infinite',
+        'spin-slower': 'spin 6s linear infinite',
+        'spin-reverse': 'spin-reverse 1s linear infinite',
+        'spin-reverse-slow': 'spin-reverse 4s linear infinite',
+        'spin-reverse-slower': 'spin-reverse 6s linear infinite',
+      },
+      keyframes: {
+        'spin-reverse': {
+          to: {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      maxWidth: {
+        '2xl': '40rem',
+      },
     },
   },
   plugins: [],
